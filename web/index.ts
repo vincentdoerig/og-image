a5662b3d2a6ca92dbcf98a3247743ad2b46eb943
Vincent <dvincdt@gmail.com>
2020-12-20 15:22:16 +0100

    use my images

--- web/index.ts
+++ web/index.ts
@@ -91,7 +91,7 @@ interface FieldProps {
 const Field = ({ label, input }: FieldProps) => {
     return H('div',
         { className: 'field' },
-        H('label', 
+        H('label',
             H('div', {className: 'field-label'}, label),
             H('div', { className: 'field-value' }, input),
         ),
@@ -142,16 +142,13 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Vincent', value: 'https://cloud-35qqpw7iw.vercel.app/0vincent.svg' },
+    { text: 'Toolbox', value: 'https://cloud-amhepgj6c.vercel.app/0toolbox-black.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
-
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Vincent', value: 'https://cloud-35qqpw7iw.vercel.app/0vincent.svg' },
+    { text: 'Toolbox', value: 'https://cloud-qkitdrect.vercel.app/0toolbox-white.svg' },
 ];
 
 const widthOptions = [
